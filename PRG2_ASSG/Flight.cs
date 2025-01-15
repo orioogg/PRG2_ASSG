f106912eb73292552750069f8390c6aac1ddfa1b
Shi Ting Janice Oh <s10269334@connect.np.edu.sg>
2025-01-15 14:05:44 +0800

    Flight class

---
+++ PRG2_ASSG/Flight.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_ASSG
+{
+    abstract class Flight
+    {
+        public string FlightNumber { get; set; }
+
+        public string Origin { get; set; }
+
+        public string Destination { get; set; }
+
+        public DateTime ExpectedTime { get; set; }
+
+        public string Status { get; set; }
+
+        //constructor
+
+        public Flight(string flightNumber, string origin, string destination, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            Status = status;
+            ExpectedTime = DateTime.Now;
+        }
+
+        public abstract double CalculateFees();
+
+        public override string ToString()
+        {
+            return "Flight Number: " + FlightNumber + 
+                   "Origin: " + Origin + 
+                   "Destination: " + Destination + 
+                   "Status: " + Status + 
+                   "Expected Time: " + ExpectedTime;
+        }
+    }
+}
--- PRG2_ASSG/Program.cs
+++ PRG2_ASSG/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine( "SS");
 
-Console.WriteLine("HELLO");
-Console.WriteLine( "SS");
 
