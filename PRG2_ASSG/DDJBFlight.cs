ac7d144d22e08958979a42405834a5066672c0ff
Shi Ting Janice Oh <s10269334@connect.np.edu.sg>
2025-01-15 15:17:03 +0800

    CFFTFlight

---
+++ PRG2_ASSG/DDJBFlight.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_ASSG
+{
+    class DDJBFlight : Flight
+    {
+        public double RequestFee { get; set; }
+
+        public Flight(string flightNumber, string origin, string destination, string status, DateTime expectedTime, double requestFee) : base(flightNumber, origin, destination, status, expectedTime)
+        {
+            RequestFee = requestFee;
+        }
+
+        //public override double CalculateFee()
+        public override string ToString()
+        {
+            return base.ToString() + "Request Fee: " + RequestFee;
+        }
+    }
+}
