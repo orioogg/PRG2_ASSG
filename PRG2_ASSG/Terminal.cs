dd9eef5ed3dd3ca1b0115ef6349b6fc635f37d01
Kah Weng Murray Wong <s10270448@connect.np.edu.sg>
2025-01-15 15:25:41 +0800

    student name

--- PRG2_ASSG/Terminal.cs
+++ PRG2_ASSG/Terminal.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//==========================================================
+// Student Number	: S10270448
+// Student Name	: Murray Wong Kah Weng
+// Partner Name	: Janice Oh Shi Ting
+//==========================================================
 
 namespace PRG2_ASSG
 {
