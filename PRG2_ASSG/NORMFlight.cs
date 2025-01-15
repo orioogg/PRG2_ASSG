4c9db349d50acb05ee0b909ed7f2cd7779b57558
Shi Ting Janice Oh <s10269334@connect.np.edu.sg>
2025-01-15 15:27:37 +0800

    files added

--- PRG2_ASSG/NORMFlight.cs
+++ PRG2_ASSG/NORMFlight.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//==========================================================
+// Student Number : S10269334
+// Student Name : Janice Oh Shi Ting
+// Partner Name : Murray Wong Kah Weng
+//==========================================================
 namespace PRG2_ASSG
 {
     class NORMFlight : Flight
